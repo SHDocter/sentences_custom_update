88d07d72d8745f5bfff3d7a17db84c8f1384689a
狐日泽 <shdocter@nya-wsl.com>
2023-05-24 18:15:55 +0800

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# sentences_custom_update
+# Sentences Custom Update
+
+![python](https://img.shields.io/badge/Version-1.0.0-cyan) ![python](https://img.shields.io/badge/Python-3.11.0-blue) ![os](https://img.shields.io/badge/OS-All-orange)
+
 bot插件扩展，用于快速更新语录库
+
+#### 注：本仓库经过脱敏处理，如需使用请修改源码中的敏感部分
---
+++ scu.py
@@ -0,0 +1,128 @@
+import uuid
+import json
+import paramiko
+from urllib import request
+
+version = "1.0.0"
+LatestVersion = "1.0.0"
+print(f"当前版本：{version} 最新版本：{LatestVersion}")
+
+def Choose():
+    global choose
+    global author
+    global SentencesFile
+    url = "http://xxx.com/" # 语录的获取url，需自行修改web服务器配置，脱敏处理，需保留末尾的“/”
+    JsonUrl = "" # 留空
+    choose = input("""
+1.桑吉
+2.羽月
+3.楠桐
+请选择您所需要添加的语录并输入序号后按回车确认：
+""")
+
+    SentencesFile = ""
+    if choose == "1":
+        JsonUrl = url + "a.json" # 语录的获取url
+        request.urlretrieve(JsonUrl,"a_local.json") # 下载语录到根目录
+        SentencesFile = "a.json" # 更新后的语录文件名，建议与服务器上的文件名字保持一致
+    elif choose == "2":
+        JsonUrl = url + "b.json"
+        request.urlretrieve(JsonUrl,"b_local.json")
+        SentencesFile = "b.json"
+    elif choose == "3":
+        author = input("谁说的：") # 获取作者
+        JsonUrl = url + "c.json"
+        request.urlretrieve(JsonUrl,"c_local.json")
+        SentencesFile = "c.json"
+    else:
+        input("该语录不存在，请检查！")
+        Choose() # 返回选择
+
+def InputSentences():
+    sentence = input("""请输入需要添加的语录内容并按回车确认：
+""")
+    item_dict = "" # 留空
+    OpenJsonFile = "" # 留空
+    if choose == "1":
+        OpenJsonFile = "a_local.json" # 与上方request的文件名一致
+    if choose == "2":
+        OpenJsonFile = "b_local.json"
+    if choose == "3":
+        OpenJsonFile = "c_local.json"
+    f = open(OpenJsonFile, 'r', encoding="utf-8") # 将语言文件写入缓存
+    text = f.read() # 读取语言
+    f.close() # 关闭语言文件
+    content = json.loads(text) # 转为List，List中为字典
+    id = len(content) + 1 # 获取字典位数并加1的方式自动更新id
+    Uuid = str(uuid.uuid4()) # 基于随机数生成uuid，可能会有极小的概率重复
+    if choose == "1":
+        item_dict = {
+    "id": f"{id}", # 新的id，通过此方式写入双引号
+    "uuid": f"{Uuid}", # 新的uuid，通过此方式写入双引号
+    "hitokoto": f"{sentence}", # 需要添加的语录将填入这里，通过此方式写入双引号
+    "type": "a",
+    "from": "资本家聚集地",
+    "from_who": "桑吉Sage",
+    "creator": "桑吉Sage",
+    "creator_uid": "1",
+    "reviewer": "1",
+    "commit_from": "web",
+    "created_at": "1626590063",
+    "length": "19"
+} # 需添加的对象
+    elif choose == "2":
+        item_dict = {
+    "id": f"{id}",
+    "uuid": f"{Uuid}",
+    "hitokoto": f"{sentence}",
+    "type": "a",
+    "from": "羽月ちい",
+    "from_who": "羽月ちい",
+    "creator": "羽月ちい",
+    "creator_uid": "1",
+    "reviewer": "1",
+    "commit_from": "web",
+    "created_at": "1626590063",
+    "length": "19"
+}
+    elif choose == "3":
+        item_dict = {
+    "id": f"{id}",
+    "uuid": f"{Uuid}",
+    "hitokoto": f"{sentence}",
+    "type": "a",
+    "from": f"{author}", # 填入作者，通过此方式写入双引号
+    "from_who": f"{author}",
+    "creator": f"{author}",
+    "creator_uid": "1",
+    "reviewer": "1",
+    "commit_from": "web",
+    "created_at": "1626590063",
+    "length": "19"
+}
+    content.append(item_dict) # 将字典追加入列表
+
+    with open(SentencesFile, 'w', encoding="utf-8") as JsonFile:
+        json.dump(content, JsonFile, indent=4, ensure_ascii=False) # 打开并写入json中，保持4格缩进并避免中文乱码
+
+# 连接远程服务器
+def UpdateSentences():
+# """
+# :param ip: 服务器ip地址
+# :param port: 端口(22)
+# :param user: 用户名
+# :param password: 用户密码
+# :param SentencesFile: 文件路径，需保存至根目录
+# :param SentencesRemotePath: 要上传的文件地址（例：/home/nyawsl/a.json）
+# """
+    SentencesRemotePath = "服务器中语录文件的保存路径" + SentencesFile
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(ip, port, user, password) # 脱敏处理，需自行修改
+    
+    sftp = ssh.open_sftp()
+    sftp.put(SentencesFile, SentencesRemotePath) #通过ssh连接上传文件至服务器
+
+Choose()
+InputSentences()
+UpdateSentences()
