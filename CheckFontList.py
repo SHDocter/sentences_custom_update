138b0715039e652a41843a7276a1449f3ab8fce5
狐日泽 <shdocter@nya-wsl.com>
2023-06-28 00:49:33 +0800

    init

--- CheckFontList.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-import tkinter.font as tkFont
-
-root = tk.Tk()
-
-FontFamilyList = tkFont.families(root=root)
-input(FontFamilyList)
--- img.py
+++
@@ -1,9 +0,0 @@
-import base64
-open_icon = open("Nya-WSL.ico","rb")
-b64str = base64.b64encode(open_icon.read())
-open_icon.close()
-write_data = f"img = {b64str}"
-print(write_data)
-f = open("icon.py","w+")
-f.write(write_data)
-f.close()
