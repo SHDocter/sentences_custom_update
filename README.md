e9b73b49fa608672d698da259e89421bcb964120
狐日泽 <shdocter@nya-wsl.com>
2023-07-06 05:03:51 +0800

    v1.0.0

--- README.md
+++ README.md
@@ -1,62 +1,19 @@
 # Sentences Custom Update
 
-![python](https://img.shields.io/badge/Version-1.4.1-cyan) ![python](https://img.shields.io/badge/Python-3.11.4-blue) ![os](https://img.shields.io/badge/OS-remote|Windows-orange) ![os](https://img.shields.io/badge/OS-local|All-orange)
+![version](https://img.shields.io/badge/Version-1.0.0-cyan) ![python](https://img.shields.io/badge/Python-3.8.10-blue)
 
 bot插件扩展，用于快速更新语录库
 
 ## 如何使用
 
-#### server
+![help](help.jpg)
 
-- 安装并运行pm2
+## ChangeLog
 
-```
-cd /root/hitokoto-api
-npm i -g pm2
-pm2 startup
-pm2 start restart.config.js # 出于安全考虑 redis需改为仅本地访问并添加密码 所以需要在服务器持久化进行刷新数据库的操作 同时上传的语录保存位置不能为语录库 该部分代码需自行编写 需要在配置文件中设置watch语录文件的变更并取消自动重启
-pm2 start ecosystem.config.js --env production # 需要在配置文件中设置watch语录文件的变更
-pm2 save
-```
+#### 1.0.1 | todo
 
-#### scu_local
+##### change:
 
-- 运行程序
+- 重启数据库和语录系统的指令改为通过bot执行而不是通过pm2监测文件变动
 
-`python3 scu_local.py`
-
-#### scu_remote
-
-- Windows
-
-`scu_remote.exe`
-
-- Linux
-
-```
-# 未测试
-# 目前版本无法使用，将在后续更新中考虑适配
-
-pip install -r requirements.txt
-python3 scu_remote_cmd.py # 因为scu_remote_gui是为Windows设计的，不推荐在Linux中使用scu_remote_gui，且无法在非desktop中运行
-```
-
-- Mac
-
-```
-# 未测试
-# 目前版本无法使用，将在后续更新中考虑适配
-
-pip install -r requirements.txt
-python3 scu_remote_cmd.py # 因为scu_remote_gui是为Windows设计的，不推荐在Mac中使用scu_remote_gui
-```
-
-#### Web | 开发中
-
-- Server
-
-`...`
-
-- Client
-
-[Nya-WSL | 语录上传系统](http://upload-yulu.nya-wsl.cn)
+#### 1.0.0 | 2023.7.6
--- scu_bot.py
+++ scu_bot.py
@@ -1,3 +1,5 @@
+import json
+import uuid
 from nonebot import on_command
 from services.log import logger
 from nonebot.adapters.onebot.v11 import Bot, Message, MessageEvent, GroupMessageEvent
@@ -12,8 +14,11 @@ usage：
     指令：
         上传语录 语录名称 语录内容
         上传语录 语录名称 语录内容 语录作者（目前仅限楠桐语录需要填写作者）
-        例：上传语录 桑吉 人家45
-        例：上传语录 楠桐 我是楠桐 晨于曦Asahi
+        
+        语录内容不能有空格
+        
+        例：上传语录 桑吉/桑吉语录 人家45
+        例：上传语录 楠桐/楠桐语录 我是楠桐 晨于曦Asahi
 """.strip()
 __plugin_des__ = "上传语录"
 __plugin_cmd__ = ["上传语录"]
@@ -32,14 +37,104 @@ UploadSentence = on_command("上传语录", aliases={"上传语录"}, priority=5
 
 @UploadSentence.handle()
 async def _(bot: Bot, event: MessageEvent, state: T_State, arg: Message = CommandArg()):
+    global SentenceName
+    global sentence
+    global author
     msg = arg.extract_plain_text().strip().split()
     if len(msg) < 2:
-        await UploadSentence.finish("参数不完全，请查看订阅帮助...")
+        await UploadSentence.finish("参数不完全，请使用'！帮助上传语录'查看帮助...")
     SentenceName = msg[0]
-    
-    result = f'已成功将{author}说的{sentence}上传至{SentenceName}语录'
+    sentence = msg[1]
+    if SentenceName in ["楠桐","楠桐语录"]:
+        try:
+            author = msg[2]
+        except:
+            await UploadSentence.finish("作者获取异常！")
+
+    if SentenceName in ["楠桐","楠桐语录"]:
+        if SentenceName == "楠桐":
+            result = f'已成功将{author}说的{sentence}上传至{SentenceName}语录'
+        else:
+            result = f'已成功将{author}说的{sentence}上传至{SentenceName}'
+    else:
+        if SentenceName in ["桑吉","羽月"]:
+            result = f'已成功将{sentence}上传至{SentenceName}语录'
+        else:
+            result = f'已成功将{sentence}上传至{SentenceName}'
+    Upload()
     await UploadSentence.send(result)
     logger.info(
         f"(USER {event.user_id}, GROUP {event.group_id if isinstance(event, GroupMessageEvent) else 'private'}) 上传语录:"
         + result
     )
+
+def Upload():
+    path = "/scu/" # 语录的路径
+    SentencesFile = "" # 留空
+
+    SentencesFile = ""
+    if SentenceName in ["桑吉","桑吉语录"]:
+        SentencesFile = path + "a.json" # 语录文件
+    elif SentenceName in ["羽月","羽月语录"]:
+        SentencesFile = path + "b.json"
+    elif SentenceName in ["楠桐","楠桐语录"]:
+        SentencesFile = path + "c.json"
+    else:
+        UploadSentence.finish("该语录不存在！")
+
+    item_dict = "" # 留空
+    f = open(SentencesFile, 'r', encoding="utf-8") # 将语言文件写入缓存
+    text = f.read() # 读取语言
+    f.close() # 关闭语言文件
+    content = json.loads(text) # 转为List，List中为字典
+    id = len(content) + 1 # 获取字典位数并加1的方式自动更新id
+    Uuid = str(uuid.uuid4()) # 基于随机数生成uuid，可能会有极小的概率重复
+    if SentenceName in ["桑吉","桑吉语录"]:
+        item_dict = {
+    "id": f"{id}", # 新的id，通过此方式写入双引号
+    "uuid": f"{Uuid}", # 新的uuid，通过此方式写入双引号
+    "hitokoto": f"{sentence}", # 需要添加的语录将填入这里，通过此方式写入双引号
+    "type": "a",
+    "from": "资本家聚集地",
+    "from_who": "桑吉Sage",
+    "creator": "桑吉Sage",
+    "creator_uid": "1",
+    "reviewer": "1",
+    "commit_from": "web",
+    "created_at": "1626590063",
+    "length": "19"
+} # 需添加的对象
+    elif SentenceName in ["羽月","羽月语录"]:
+        item_dict = {
+    "id": f"{id}",
+    "uuid": f"{Uuid}",
+    "hitokoto": f"{sentence}",
+    "type": "b",
+    "from": "羽月ちい",
+    "from_who": "羽月ちい",
+    "creator": "羽月ちい",
+    "creator_uid": "1",
+    "reviewer": "1",
+    "commit_from": "web",
+    "created_at": "1626590063",
+    "length": "19"
+}
+    elif SentenceName in ["楠桐","楠桐语录"]:
+        item_dict = {
+    "id": f"{id}",
+    "uuid": f"{Uuid}",
+    "hitokoto": f"{sentence}",
+    "type": "c",
+    "from": f"{author}", # 填入作者，通过此方式写入双引号
+    "from_who": f"{author}",
+    "creator": f"{author}",
+    "creator_uid": "1",
+    "reviewer": "1",
+    "commit_from": "web",
+    "created_at": "1626590063",
+    "length": "19"
+}
+    content.append(item_dict) # 将字典追加入列表
+
+    with open(SentencesFile, 'w', encoding="utf-8") as JsonFile:
+        json.dump(content, JsonFile, indent=4, ensure_ascii=False) # 打开并写入json中，保持4格缩进并避免中文乱码
