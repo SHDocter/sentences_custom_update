6e81fef13aa7a30f93c73ab313478669d5ef365c
狐日泽 <shdocter@nya-wsl.com>
2023-07-27 13:18:24 +0800

    shell更新
    - 适配AGPL协议，上传语录时会将语录同步推送至仓库

---
+++ git.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cp -r /root/sentences/sentences/*.json /root/hitokoto/
+
+function git_command {
+git add .
+git commit -am "update"
+git push
+}
+
+cd /root/hitokoto
+git_command
--- restart.sh
+++ restart.sh
@@ -6,3 +6,6 @@ redis-cli -a "your redis passwd" --no-auth-warning flushall
 pm2 restart 0 # pm2中语录系统的id，一般是固定的，也可直接写name
 time=$(date "+%Y%m%d-%H:%M:%S")
 echo "${time}"
+cd /root/hitokoto/
+./git.sh
+echo "已执行推送"
