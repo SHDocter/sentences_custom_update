6c55bf33954b89a44d17bfe53c65398c5178d238
狐日泽 <shdocter@nya-wsl.com>
2023-09-27 15:26:26 +0800

    彩蛋更新
    - 优化初始化流程
    - 现在判断用户将从配置文件中获取列表，不再是写死在程序中
    - 移除logger输出
    - 出现错误将不再通过logger抛出错误

--- i_think_is_ok/__init__.py
+++ i_think_is_ok/__init__.py
@@ -3,7 +3,7 @@ Author: Nya-WSL
 Copyright © 2023 by Nya-WSL All Rights Reserved. 
 Date: 2023-09-25 21:46:47
 LastEditors: 狐日泽
-LastEditTime: 2023-09-26 21:25:24
+LastEditTime: 2023-09-27 14:55:59
 '''
 from nonebot import on_keyword
 from services.log import logger
@@ -33,31 +33,47 @@ __plugin_usage__ = """
 usage：
     A：我觉得行
     bot：你行不了一点
+    
+install：
+    只有group_list.json中存在的群才会触发，该文件初次运行将会自动生成
+    除了我觉得行功能以外，其他用户限定功能发送人qq号或者id需要存在于user_list.json中相关的键的值（值可以是列表指定多个用户，也可以是字符串指定单个用户）才能触发
 """.strip()
 
 send_img = on_keyword({"yhm", "樱花妹", "我觉得行"}, priority=5, block=True)
 
 ImagePath = IMAGE_PATH / "scu/easter_egg"
 ResourcesPath = Path() / "custom_plugins" / "i_think_is_ok"
-ImgPath1 = ResourcesPath / "1.jpg"
-ImgPath2 = ResourcesPath / "yhm.jpg"
-ListPath = ResourcesPath / "group_list.json"
+ImgPath = ResourcesPath / "img"
+GroupListPath = ResourcesPath / "group_list.json"
+UserListPath = ResourcesPath / "user_list.json"
 
 if not os.path.exists(ImagePath):
     os.mkdir(ImagePath)
-    os.system(f"cp -rf {ImgPath1} {ImagePath}")
-    os.system(f"cp -rf {ImgPath2} {ImagePath}")
+    os.system(f"cp -rf {ImgPath}/* {ImagePath}")
 
-if not ListPath.exists():
-    with open(ListPath, "w", encoding="utf-8") as gl:
+if not GroupListPath.exists():
+    with open(GroupListPath, "w", encoding="utf-8") as gl:
         gl.write(r"[]")
+
+if not UserListPath.exists():
+    with open(UserListPath, "w", encoding="utf-8") as ul:
+        UserList = [
+    {
+        "yhm": ["0000000000"],
+        "example": "example"
+    }
+]
+        json.dump(UserList, ul, ensure_ascii=False, indent=4)
 
 @send_img.handle()
 async def _(event: MessageEvent):
-    with open(ListPath, "r", encoding="utf-8") as gl:
+    with open(GroupListPath, "r", encoding="utf-8") as gl:
         GroupList = json.load(gl)
+    with open(UserListPath, "r", encoding="utf-8") as ul:
+        UserList = json.load(ul)
     if f"{event.group_id}" in GroupList:
-        if f"{event.user_id}" in ["1040723300"]:
+        # 用户限定功能1
+        if f"{event.user_id}" in UserList["yhm"]:
             if re.search("yhm", str(event.message)) or re.search("樱花妹", str(event.message)):
                 ImgList = fnmatch.filter(os.listdir(ImagePath), "asahi*.*")
                 RandomImg = random.choice(ImgList)
@@ -65,29 +81,13 @@ async def _(event: MessageEvent):
                 await send_img.send(result)
                 flush = gc.collect()
                 print(f"已成功清理内存：{flush}")
+        # 全局功能，不限用户
         if f"{event.message}" == "我觉得行":
             length = len(os.listdir(ImagePath))
             if length == 0:
                 logger.warning(f"彩蛋图库为空，调用取消！")
                 await send_img.finish("哥们没活了呜呜呜")
             result = image("scu/easter_egg/" + "1.jpg")
-            if result:
-                logger.info(
-                    f"发送:" + result,
-                    "发送图片",
-                    event.user_id,
-                    getattr(event, "group_id", None),
-                )
-                await send_img.send(result)
-                flush = gc.collect()
-                print(f"已成功清理内存：{flush}")
-            else:
-                logger.info(
-                    f"发送失败",
-                    "发送图片",
-                    event.user_id,
-                    getattr(event, "group_id", None),
-                )
-                await send_img.finish(f"发生错误！")
-                flush = gc.collect()
-                print(f"已成功清理内存：{flush}")
+            await send_img.send(result)
+            flush = gc.collect()
+            print(f"已成功清理内存：{flush}")
